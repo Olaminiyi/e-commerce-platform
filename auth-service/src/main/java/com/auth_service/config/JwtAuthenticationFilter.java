6b1e3b3b5a0fc574f34bc7baa4753f4991524271
olaminiyi <olaiyaolaminiyi@gmail.com>
2025-05-24 02:28:55 +0100

    added filter class

---
+++ auth-service/src/main/java/com/auth_service/config/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.auth_service.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final  String authHeader = request.getHeader("Authorization");
+        final  String jwt;
+        final  String userEmail;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+
+    }
+}
---
+++ auth-service/src/main/java/com/auth_service/user/Role.java
@@ -0,0 +1,7 @@
+package com.auth_service.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
