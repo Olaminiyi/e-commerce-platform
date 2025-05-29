cf14de2e0d0152b947b155eb18f1059f4053289c
olaminiyi <olaiyaolaminiyi@gmail.com>
2025-05-29 02:31:18 +0100

    added more functions

--- auth-service/src/main/java/com/auth_service/config/JwtService.java
+++ auth-service/src/main/java/com/auth_service/config/JwtService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -55,5 +56,23 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(key, Jwts.SIG.HS256)
                 .compact();
+    }
+
+    // generate token with just username
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public  boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 }
