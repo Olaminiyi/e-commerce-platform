6fb04e50a1cca4005a56634ce91f1e90cd61cff6
olaminiyi <olaiyaolaminiyi@gmail.com>
2025-06-02 23:26:14 +0100

    added authentication controller

---
+++ auth-service/src/main/java/com/auth_service/auth/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.auth_service.auth;
+
+import com.auth_service.config.JwtService;
+import com.auth_service.user.Role;
+import com.auth_service.user.User;
+import com.auth_service.user.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private  final PasswordEncoder passwordEncoder;
+    private final JwtService service;
+    private  final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = service.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticateRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                // make sure you catch the exception
+                .orElseThrow();
+        var jwtToken = service.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
