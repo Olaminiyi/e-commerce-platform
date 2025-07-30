d4b7b66a874bfcd68db225962fd5cb51c109631e
olaminiyi <olaiyaolaminiyi@gmail.com>
2025-07-30 01:58:40 +0100

    service layer

--- product-service/src/main/java/com/product_service/service/ProductService.java
+++ product-service/src/main/java/com/product_service/service/ProductService.java
@@ -5,13 +5,21 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import product_service.baseService.BaseServiceImpl;
+import product_service.dto.ProductRequest;
+import product_service.dto.ProductResponse;
 import product_service.exception.model.ProductServiceException;
+import product_service.mapper.ProductMapper;
 import product_service.model.Category;
 import product_service.model.Product;
 import product_service.model.Tag;
 import product_service.repository.CategoryRepository;
 import product_service.repository.ProductRepository;
 import product_service.repository.TagRepository;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ProductService extends BaseServiceImpl<Product, Long> {
@@ -27,10 +35,31 @@ public class ProductService extends BaseServiceImpl<Product, Long> {
         this.tagRepository = tagRepository;
     }
 
-    @Override
-    public Page<Product> findAll(Pageable pageable){
 
-        return null;
+    public Page <ProductResponse> getAllProducts(Pageable pageable){
+        Page<Product> products = productRepository.findAll(pageable);
+        return products.map(ProductMapper::toResponse);
+    }
+
+    public ProductResponse CreateProduct(ProductRequest request){
+       Category category = categoryRepository.findById(request.getCategoryId())
+               .orElseThrow(() -> new ProductServiceException("Category not found", HttpStatus.NOT_FOUND.value()));
+
+        Set<Tag> tags = new HashSet<>(tagRepository.findAllById(request.getTagIds()));
+
+        Product newproduct = Product.builder()
+                .name(request.getName())
+                .specifications(request.getSpecifications())
+                .category(category)
+                .tags(tags)
+                .price(request.getPrice())
+                .description(request.getDescription())
+                .available(true)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        productRepository.save(newproduct);
+        return ProductMapper.toResponse(newproduct);
     }
 
     public Page<Product> search (Long categoryId, Long tagId, String keyword, Pageable pageable){
