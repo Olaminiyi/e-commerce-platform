069d9be653758f57f953ea85c4c09990ea64d285
Olaminiyi <olaiyaolaminiyi@gmail.com>
2025-08-14 23:13:48 +0100

    Update BaseService.java

--- product-service/src/main/java/com/product_service/baseService/BaseService.java
+++ product-service/src/main/java/com/product_service/baseService/BaseService.java
@@ -1,7 +1,6 @@
 package product_service.baseService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import java.util.Optional;
 
 public interface BaseService<T, ID> {
