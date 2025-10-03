e3d19e1160b24828005bf8e85e978c3a6a947eb7
Olaminiyi <olaiyaolaminiyi@gmail.com>
2025-10-03 15:25:05 +0100

    Update BaseService.java

--- product-service/src/main/java/com/product_service/baseService/BaseService.java
+++ product-service/src/main/java/com/product_service/baseService/BaseService.java
@@ -3,6 +3,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
+
 public interface BaseService<T, ID> {
     T save(T entity);
     Optional<T> findById(ID id);
